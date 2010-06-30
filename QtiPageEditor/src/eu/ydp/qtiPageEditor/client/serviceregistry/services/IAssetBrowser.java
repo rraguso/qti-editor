da1d7eeec9617912c7f5f1adb8cd06e2cd44c112
thanczewski <thanczewski@b23be626-c637-cee4-8df3-8a3a905a95a0>
2010-06-30 09:32:08 +0000

    setSelectedFile(String fileName) method added to AssetBrowser bug 33441

--- QtiPageEditor/src/eu/ydp/qtiPageEditor/client/serviceregistry/services/IAssetBrowser.java
+++ QtiPageEditor/src/eu/ydp/qtiPageEditor/client/serviceregistry/services/IAssetBrowser.java
@@ -7,6 +7,7 @@ public interface IAssetBrowser {
 	void browse(AssetBrowserCallback browseCallback );
 	void browse(AssetBrowserCallback browseCallback, String[] fileFilter );
 	void setSelectedAssetPath(String filePath);
+	void setSelectedFile(String fileName);
 	String getSelectedAssetPath();
 
 }
