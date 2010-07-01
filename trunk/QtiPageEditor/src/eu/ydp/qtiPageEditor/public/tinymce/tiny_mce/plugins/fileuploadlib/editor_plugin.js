69ea866fa030edfb5b2fdfcb69f6eda8fc7a9eb8
ulff.pl <ulff.pl@b23be626-c637-cee4-8df3-8a3a905a95a0>
2010-07-01 08:52:00 +0000

    rebuilded all uploads bug 33407

---
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/fileuploadlib/editor_plugin.js
@@ -0,0 +1,91 @@
+(function() {
+
+	tinymce.create('tinymce.plugins.fileUploadLib', {
+		
+		init : function(ed, url) {
+			
+			ed.addCommand('mceAppendImageToPage', function(ui, src) {
+				
+				var browseCallback = {
+				
+					onBrowseComplete : function(filePath) {
+						var imgTag = '<img src="' + filePath + '" border="0" />';
+						ed.selection.moveToBookmark(ed.selection.getBookmark());
+						tinyMCE.execCommand('mceInsertContent', false, imgTag);
+						return true;
+					},
+					
+					onBrowseError : function(error) {
+						alert(error);
+						return false;
+					}
+					
+				}
+				
+				var extensions = [".jpg",".jpeg", ".gif", ".bmp", ".png"];
+				
+				var assetBrowser = tinyMCE.gwtProxy.getAssetBrowser();
+				
+				if(src != undefined && src != '') {
+					srcArr = src.split('/');
+					fileName = String(srcArr[srcArr.length-1]);
+					assetBrowser.setSelectedFile(fileName);
+				}
+				
+				assetBrowser.browse(browseCallback, extensions);
+				
+			});
+			
+			ed.addCommand('mceAppendImageToExercise', function(ui,data) {
+			
+				var browseCallback = {
+				
+					onBrowseComplete : function(filePath) {
+						var imgSrc = $('#thumb > img').attr('src');
+						data.div.innerHTML = '<img style="max-height: 40px; max-width: 80px;" src="' + filePath + '">';
+						data.div.previousSibling.setAttribute('value',filePath);
+						return true;
+					},
+					
+					onBrowseError : function(error) {
+						alert(error);
+						return false;
+					}
+					
+				}
+				
+				var extensions = [".jpg",".jpeg", ".gif", ".bmp", ".png"];
+				
+				var assetBrowser = tinyMCE.gwtProxy.getAssetBrowser();
+				
+				if(data.src != undefined && data.src != '') {
+					srcArr = src.split('/');
+					fileName = String(srcArr[srcArr.length-1]);
+					assetBrowser.setSelectedFile(fileName);
+				}
+				
+				assetBrowser.browse(browseCallback, extensions);
+			
+			});
+			
+			ed.addButton('fileuploadlib_image', {
+				title : 'Upload / insert image',
+				cmd : 'mceAppendImageToPage',
+				image : url + '/img/imglib.png'
+			});
+			
+		},
+
+		getInfo : function() {
+			return {
+				longname : 'Plugin managing uploads',
+				author : 'Young Digital Planet',
+				authorurl : '',
+				infourl : '',
+				version : "1.0"
+			};
+		}
+	});
+
+	tinymce.PluginManager.add('fileuploadlib', tinymce.plugins.fileUploadLib);
+})();
