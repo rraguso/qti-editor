fb1f7c66145d58b054ff3d60c698ab0ffc8bdcd1
thanczewski <thanczewski@b23be626-c637-cee4-8df3-8a3a905a95a0>
2010-07-12 12:32:39 +0000

    Dialog for page remove bug 33904

--- QtiPageEditor/src/eu/ydp/qtiPageEditor/client/view/component/yesno/YesNoDialog.java
+++ QtiPageEditor/src/eu/ydp/qtiPageEditor/client/view/component/yesno/YesNoDialog.java
@@ -1,47 +1,36 @@
-package eu.ydp.qtiPageEditor.client.view.component;
+package eu.ydp.qtiPageEditor.client.view.component.yesno;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import eu.ydp.qtiPageEditor.client.events.DialogYesNoEvent;
 import eu.ydp.qtiPageEditor.client.events.handler.DialogYesNoHandler;
 
-public class YesNoDialog extends DialogBox implements ClickHandler {
+public class YesNoDialog extends DialogBox {
+	
+	interface YesNoDialogBinder extends UiBinder<Widget, YesNoDialog> {}
+	private static YesNoDialogBinder uiBinder = GWT.create(YesNoDialogBinder.class);	
 	
 	final private HandlerManager _handlerManager = new HandlerManager(this);
-	private Button _okButton;
-	private Button _cancelButton;
+
+	@UiField Button _okButton;
+	@UiField Button _cancelButton;
+	@UiField HTML _text;
 	
-	private HTML _text;
+	
 	
 	public YesNoDialog(){
 		super(false, true);
-		setGlassEnabled(true);
-		
-		VerticalPanel contentPane = new VerticalPanel();
-		contentPane.setSpacing(4);
-		
-		HorizontalPanel buttonsContainer = new HorizontalPanel();
-		
-		_okButton = new Button("OK",this);
-		_cancelButton = new Button("Cancel",this);
-		
-		buttonsContainer.add(_okButton);
-		buttonsContainer.add(_cancelButton);
-		buttonsContainer.setCellWidth(_okButton, "100%");
-		
-		_text = new HTML();
-		contentPane.add(_text);
-		contentPane.add(buttonsContainer);
-		
-		setWidget(contentPane);
-				
+		setGlassEnabled(true);		
+		setWidget(uiBinder.createAndBindUi(this));				
 	}
 	
 	public void showDialog(String title, String message){
@@ -56,10 +45,19 @@ public class YesNoDialog extends DialogBox implements ClickHandler {
 		this.center();
 	}
 	
-	@Override
-	public void onClick(ClickEvent event){
+	@UiHandler("_okButton")
+	protected void onOkClick(ClickEvent event){
+		doOnClick((Button)event.getSource());
+	}
+	
+	@UiHandler("_cancelButton")
+	protected void onOkCancel(ClickEvent event){
+		doOnClick((Button)event.getSource());
+	}
+	
+	private void doOnClick(Button butt){
 		DialogYesNoEvent dialogEvent;		
-		String res = (event.getSource().equals(_okButton)) ? DialogYesNoEvent.DIALOG_YES : DialogYesNoEvent.DIALOG_NO;
+		String res = (butt.equals(_okButton)) ? DialogYesNoEvent.DIALOG_YES : DialogYesNoEvent.DIALOG_NO;
 		
 		dialogEvent = new DialogYesNoEvent(res);
 		_handlerManager.fireEvent(dialogEvent);	
