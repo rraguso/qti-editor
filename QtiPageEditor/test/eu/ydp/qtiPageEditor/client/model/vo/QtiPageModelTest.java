520e5f037245698807a15fc600a556915e5101db
thanczewski <thanczewski@b23be626-c637-cee4-8df3-8a3a905a95a0>
2010-07-13 10:48:59 +0000

    model refactoring bug 33858

--- QtiPageEditor/test/eu/ydp/qtiPageEditor/client/model/vo/QtiPageModelTest.java
+++ QtiPageEditor/test/eu/ydp/qtiPageEditor/client/model/vo/QtiPageModelTest.java
@@ -1,5 +1,6 @@
 package eu.ydp.qtiPageEditor.client.model.vo;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.junit.client.GWTTestCase;
 public class QtiPageModelTest extends GWTTestCase {
 	
@@ -11,6 +12,11 @@ public class QtiPageModelTest extends GWTTestCase {
 		return "eu.ydp.qtiPageEditor.QtiPageEditor";
 	}
 	
+	public void testGetTitle(){
+		_model = new QTIPageModel();
+		assertEquals("Unknown title", _model.getTitle());
+	}
+	
 	public void testSetTitle(){
 		_model = new QTIPageModel();
 		String title = "myTitle";		
@@ -20,6 +26,17 @@ public class QtiPageModelTest extends GWTTestCase {
 		resultTitle = _model.getTitle();
 		
 		assertEquals(resultTitle, title);	
+		
+	}
+	
+	public void testSetPath(){		
+		_model = new QTIPageModel();
+		
+		assertNull(_model.getTitle());
+		
+		_model.setPath(GWT.getModuleBaseURL());
+		
+		assertEquals(GWT.getModuleBaseURL(), _model.getPath());
 		
 	}
 
