992f747a2d91836ce42ef5582968e0e0c2daf0f2
thanczewski <thanczewski@b23be626-c637-cee4-8df3-8a3a905a95a0>
2010-07-16 05:50:49 +0000

    new player support
    bug 34029

--- trunk/QtiTestEditor/src/eu/ydp/qtitesteditor/client/controller/StartupCommand.java
+++ trunk/QtiTestEditor/src/eu/ydp/qtitesteditor/client/controller/StartupCommand.java
@@ -5,7 +5,6 @@ import org.puremvc.java.multicore.patterns.command.SimpleCommand;
 import org.puremvc.java.multicore.patterns.facade.Facade;
 
 import eu.ydp.qtiPageEditor.client.controller.startupdata.StartupData;
-import eu.ydp.qtiPageEditor.client.model.AssessmentProviderProxy;
 import eu.ydp.qtiPageEditor.client.model.QTIPageModelProxy;
 import eu.ydp.qtiPageEditor.client.model.QTITestModelProxy;
 import eu.ydp.qtitesteditor.client.view.MainViewMediator;
@@ -18,8 +17,7 @@ public class StartupCommand extends SimpleCommand {
 		
 		Facade facade = getFacade();
 		facade.registerProxy(new QTITestModelProxy());
-		facade.registerProxy(new QTIPageModelProxy());
-		facade.registerProxy(new AssessmentProviderProxy());
+		facade.registerProxy(new QTIPageModelProxy());		
 		facade.registerMediator(new MainViewMediator(startupData));
 		
 	}  
