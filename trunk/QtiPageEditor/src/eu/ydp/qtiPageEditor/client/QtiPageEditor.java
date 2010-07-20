01a743c8bb8442d8f8d2c805edadfc4fa3dce102
michal.samujlo@gmail.com <michal.samujlo@gmail.com@b23be626-c637-cee4-8df3-8a3a905a95a0>
2010-07-20 08:53:59 +0000

    in hosted mode QtiPageEditor uses RPCStorage bug 34068

--- trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/client/QtiPageEditor.java
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/client/QtiPageEditor.java
@@ -1,9 +1,7 @@
 package eu.ydp.qtiPageEditor.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.event.shared.HandlerManager;
-import com.google.gwt.user.client.DOM;
+import com.google.gwt.core.client.GWT;
 
 import eu.ydp.qtiPageEditor.client.appcallback.SaveCallback;
 import eu.ydp.qtiPageEditor.client.constance.Constances;
@@ -11,15 +9,16 @@ import eu.ydp.qtiPageEditor.client.controller.startupdata.StartupData;
 import eu.ydp.qtiPageEditor.client.env.IEditorEnvirnoment;
 import eu.ydp.qtiPageEditor.client.env.impl.EditorEnvirnoment;
 import eu.ydp.qtiPageEditor.client.events.SessionSustainerErrorEvent;
-import eu.ydp.qtiPageEditor.client.events.TinyMceSaveEvent;
 import eu.ydp.qtiPageEditor.client.events.handler.SessionSustainerErrorHandler;
 import eu.ydp.qtiPageEditor.client.model.jso.ModuleConfig;
 import eu.ydp.qtiPageEditor.client.serviceregistry.ServiceFactory;
 import eu.ydp.qtiPageEditor.client.serviceregistry.ServicesRegistry;
 import eu.ydp.qtiPageEditor.client.session.SessionSustainer;
 import eu.ydp.qtiPageEditor.client.view.component.AlertWindow;
+import eu.ydp.webapistorage.client.storage.IStorage;
 import eu.ydp.webapistorage.client.storage.apierror.IApiError;
 import eu.ydp.webapistorage.client.storage.impl.Storage;
+import eu.ydp.webapistorage.client.storage.rpc.impl.RPCStorage;
 
 
 /**
@@ -76,8 +75,16 @@ public class QtiPageEditor implements EntryPoint {
 	
 	private void register(ModuleConfig conf )
 	{		
-		ServicesRegistry sr = new ServicesRegistry(new ServiceFactory());		
-		_env = new EditorEnvirnoment(conf.getPageURL(),"media", Storage.getInstance(), sr);		
+		ServicesRegistry sr = new ServicesRegistry(new ServiceFactory());
+
+		// when in hosted mode use RPCStorage
+		Boolean isHostedMode = GWT.getPermutationStrongName().equals( GWT.HOSTED_MODE_PERMUTATION_STRONG_NAME);
+		IStorage storage = isHostedMode? RPCStorage.getInstance() : Storage.getInstance();
+		// hardcoded path to edited resources - put files in war folder to run app in hosted mode 
+		String resourcesPath = "/resources/64/script_00001/page_14.utp";
+		String pageURL = isHostedMode? resourcesPath : conf.getPageURL();
+
+		_env = new EditorEnvirnoment(pageURL,"media", storage, sr);		
 		SessionSustainer sessionSustainer = new SessionSustainer(_env.getBasePath(), _env.getStorage());
 		
 		sessionSustainer.addErrorHandler(new SessionSustainerErrorHandler() {			
