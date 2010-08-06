cd9e9c0aa0cd07e72b2b15fefc0173d55a5e01c3
michal.samujlo <michal.samujlo@gmail.com>
2010-08-06 08:02:14 +0000

    added save requests queueing by QtiProxyBase

--- QtiPageEditor/src/eu/ydp/qtiPageEditor/client/model/QTIPageModelProxy.java
+++ QtiPageEditor/src/eu/ydp/qtiPageEditor/client/model/QTIPageModelProxy.java
@@ -1,5 +1,8 @@
 package eu.ydp.qtiPageEditor.client.model;
 
+
+import com.google.gwt.user.client.Command;
+
 import eu.ydp.qtiPageEditor.client.appcallback.SaveCallback;
 import eu.ydp.qtiPageEditor.client.constants.Notifications;
 import eu.ydp.qtiPageEditor.client.model.vo.ModelPageList;
@@ -10,212 +13,214 @@ import eu.ydp.webapistorage.client.storage.callback.IResourceCallback;
 import eu.ydp.webapistorage.client.storage.callback.IResourceTextCallback;
 import eu.ydp.webapistorage.client.util.ApiErrorToJs;
 
-public class QTIPageModelProxy extends QtiProxyBase{
-	
+public class QTIPageModelProxy extends QtiProxyBase {
+
 	public static final String NAME = "qtiPagesDataModelProxy";
-	
+
 	private int _selectedIndex;
-	
+
 	private SaveCallback _jsSaveCallback;
-	
+
 	public QTIPageModelProxy() {
 		super(NAME, new ModelPageList());
 		_selectedIndex = -1;
 	}
-	
-	public void setSaveCallback(SaveCallback jsSaveCallback){
+
+	public void setSaveCallback(SaveCallback jsSaveCallback) {
 		_jsSaveCallback = jsSaveCallback;
 	}
-	
-	public void load(String[] hrefs){		
-		if(hrefs.length > 0){		
-			//String path = _testPath.substring(0, _testPath.lastIndexOf("/")+1);
-			//String pagePath = path + hrefs[0];			
-			loadPages(hrefs);		
+
+	public void load(String[] hrefs) {
+		if (hrefs.length > 0) {
+			loadPages(hrefs);
 		}
-		
+
 	}
-	
-	private void loadPages(final String[] hrefs){
-		
-		final ModelPageList pages = getDataVO();		
-		String path = _testPath.substring(0, _testPath.lastIndexOf("/")+1);
+
+	private void loadPages(final String[] hrefs) {
+
+		final ModelPageList pages = getDataVO();
+		String path = _testPath.substring(0, _testPath.lastIndexOf("/") + 1);
 		String pagePath = path + hrefs[pages.getPageCount()];
 		IResource resource = _storage.getResource(pagePath);
 		resource.load(new IResourceTextCallback() {
-			
+
 			@Override
 			public void onRequestError(IResource resource, String command,
 					IApiError error) {
 				sendNotification(Notifications.LOAD_PAGE_ERROR, error);
-				
+
 			}
-			
-			
+
 			@Override
-			public void onRequestComplete(IResource resource, String command,String content) {
-								
+			public void onRequestComplete(IResource resource, String command,
+					String content) {
+
 				QTIPageModel page = new QTIPageModel();
 				page.setContent(content);
 				page.setPath(resource.getPath());
-				
-				pages.addPage(page);			
-				
-				if(pages.getPageCount() < hrefs.length){
+
+				pages.addPage(page);
+
+				if (pages.getPageCount() < hrefs.length) {
 					loadPages(hrefs);
-				}
-				else
-				{					
+				} else {
 					String[] titles = new String[pages.getPageCount()];
 					int i;
-					for(i = 0;i < pages.getPageCount(); i++)
+					for (i = 0; i < pages.getPageCount(); i++)
 						titles[i] = pages.getPage(i).getTitle();
-					
-					_selectedIndex = 0;		
+
+					_selectedIndex = 0;
 					sendNotification(Notifications.PAGES_LOADED, titles);
 				}
-							
+
 			}
-			
+
 		});
-	}	
-	
-	public void reload(int ix){
+	}
+
+	public void reload(int ix) {
 		final ModelPageList pages = getDataVO();
 		final QTIPageModel page = pages.getPage(ix);
-		String path = page.getPath();		
+		String path = page.getPath();
 		IResource resource = _storage.getResource(path);
 		resource.load(new IResourceTextCallback() {
-			
+
 			@Override
-			public void onRequestError(IResource resource, String command, IApiError error) {				
+			public void onRequestError(IResource resource, String command,
+					IApiError error) {
 				sendNotification(Notifications.LOAD_PAGE_ERROR, error);
 			}
-			
+
 			@Override
-			public void onRequestComplete(IResource resource, String command, String content) {				
+			public void onRequestComplete(IResource resource, String command,
+					String content) {
 				page.setContent(content);
-				
+
 				String[] titles = new String[pages.getPageCount()];
 				int i;
-				for(i = 0;i < pages.getPageCount(); i++)
+				for (i = 0; i < pages.getPageCount(); i++)
 					titles[i] = pages.getPage(i).getTitle();
-				
+
 				sendNotification(Notifications.PAGES_LOADED, titles);
 			}
 		});
 
-		
 	}
-	
-	public void setSelectedIndex(int ix){
+
+	public void setSelectedIndex(int ix) {
 		_selectedIndex = ix;
 	}
-	
-	public int getSelectedIndex(){
+
+	public int getSelectedIndex() {
 		return _selectedIndex;
 	}
-	
-	public void setPagePath(String path){
-		setPagePath(path,-1);
+
+	public void setPagePath(String path) {
+		setPagePath(path, -1);
 	}
-	
-	public void setPagePath(String path, int ix){
+
+	public void setPagePath(String path, int ix) {
 		_testPath = path;
 		ModelPageList pages = getDataVO();
 		int n = (ix > -1) ? ix : 0;
-		
+
 		pages.getPage(n).setPath(path);
-		
+
 	}
-	
-	public void addPage(String href){		
+
+	public void addPage(String href) {
 		ModelPageList pages = getDataVO();
-		
+
 		QTIPageModel page = new QTIPageModel();
-		String basePath = _testPath.substring(0, _testPath.lastIndexOf("/")+1);
+		String basePath = _testPath
+				.substring(0, _testPath.lastIndexOf("/") + 1);
 		page.setPath(basePath + href);
-		page.setTitle(href.substring(href.lastIndexOf("/")+1));
-		
-		pages.addPage(page);		
-		
+		page.setTitle(href.substring(href.lastIndexOf("/") + 1));
+
+		pages.addPage(page);
+
 	}
-	
-	public void addPage(){		
+
+	public void addPage() {
 		ModelPageList pages = getDataVO();
-		
-		QTIPageModel page = new QTIPageModel();		
-		pages.addPage(page);		
-	}	
-	
-	public void removePage(int ix){		
+
+		QTIPageModel page = new QTIPageModel();
+		pages.addPage(page);
+	}
+
+	public void removePage(int ix) {
 		getDataVO().removePage(ix);
 	}
-	
-	
-	public void swapItems(int from, int to){
-		getDataVO().swapItems(from, to);		
-	}	
 
-	public void moveDown(int ix){
-		getDataVO().moveDown(ix);
-	
+	public void swapItems(int from, int to) {
+		getDataVO().swapItems(from, to);
 	}
-	
-	public void moveUp(int ix){		
+
+	public void moveDown(int ix) {
+		getDataVO().moveDown(ix);
+
+	}
+
+	public void moveUp(int ix) {
 		getDataVO().moveUp(ix);
 	}
-	
-	
-	public ModelPageList getDataVO(){
-		return (ModelPageList)getData();
+
+	public ModelPageList getDataVO() {
+		return (ModelPageList) getData();
 	}
-	
-	public void updatePageState(int ix, String content){
+
+	public void updatePageState(int ix, String content) {
 		getDataVO().getPage(ix).setContent(content);
-	}	
-	
-	public String getPageContent(int ix){
+	}
+
+	public String getPageContent(int ix) {
 		return getDataVO().getPage(ix).getContent();
 	}
-	
-	public QTIPageModel getPage(int ix){
+
+	public QTIPageModel getPage(int ix) {
 		return getDataVO().getPage(ix);
 	}
-	
-	public QTIPageModel getCurrentPage(){		
+
+	public QTIPageModel getCurrentPage() {
 		return getDataVO().getPage(_selectedIndex);
 	}
-	
-	
-	public void save(int ix){
-		
+
+	public void save(int ix) {
+
 		String path;
 		ModelPageList pages = getDataVO();
 		QTIPageModel page = pages.getPage(ix);
-			
-		path = page.getPath();		
-		IResource resource = _storage.getResource(path);		
-		
-		resource.save(page.getContent(), new IResourceCallback() {
-			
-			@Override
-			public void onRequestError(IResource resource, String command,IApiError error) {
-				sendNotification(Notifications.SAVE_PAGE_ERROR, error);
-				if(_jsSaveCallback != null)
-					_jsSaveCallback.onSaveError(ApiErrorToJs.toJs(error));
-				
-			}
-				
-			@Override
-			public void onRequestComplete(IResource resource, String command) {
-				// TODO Auto-generated method stub
-				if(_jsSaveCallback != null)
-					_jsSaveCallback.onSaveComplete();
-									
-			}
-		});						
+
+		path = page.getPath();
+		IResource resource = _storage.getResource(path);
+
+		// encapsulate save request data in command
+		Command saveCommand = new SaveResourceCommand(resource,
+				page.getContent(), new IResourceCallback() {
+
+					@Override
+					public void onRequestError(IResource resource,
+							String command, IApiError error) {
+						sendNotification(Notifications.SAVE_PAGE_ERROR, error);
+						if (_jsSaveCallback != null)
+							_jsSaveCallback.onSaveError(ApiErrorToJs
+									.toJs(error));
+
+					}
+
+					@Override
+					public void onRequestComplete(IResource resource,
+							String command) {
+						if (_jsSaveCallback != null)
+							_jsSaveCallback.onSaveComplete();
+
+					}
+				});
+
+		scheduleSave();
+		saveQueue.add(saveCommand);
+
 	}
-	
-	
+
 }
--- QtiPageEditor/src/eu/ydp/qtiPageEditor/client/model/QtiProxyBase.java
+++ QtiPageEditor/src/eu/ydp/qtiPageEditor/client/model/QtiProxyBase.java
@@ -1,6 +1,12 @@
 package eu.ydp.qtiPageEditor.client.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.puremvc.java.multicore.patterns.proxy.Proxy;
+
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.user.client.Command;
 
 import eu.ydp.qtiPageEditor.client.env.IEditorEnvirnoment;
 import eu.ydp.webapistorage.client.storage.IStorage;
@@ -9,6 +15,15 @@ public class QtiProxyBase extends Proxy {
 	
 	protected String _testPath;
 	protected IStorage _storage;
+	/**
+	 * Proxy queues save requests to allow http server to release lock on saved files.
+	 * @see http://bugtrack/show_bug.cgi?id=33887    
+	 */
+	protected List<Command> saveQueue = new ArrayList<Command>();
+	/**
+	 * Save request from saveQueue are executed with defined timeout 
+	 */
+	protected int saveTimeout = 250;
 	
 	public QtiProxyBase(String name, Object data) {
 		super(name, data);
@@ -17,5 +32,18 @@ public class QtiProxyBase extends Proxy {
 	public void init(IEditorEnvirnoment env){
 		_testPath = env.getBasePath();
 		_storage = env.getStorage();
-	}	
+	}
+	
+	protected void scheduleSave() {
+		if (saveQueue.size()==0) {
+			Scheduler.get().scheduleFixedDelay(new Scheduler.RepeatingCommand() {
+				@Override
+				public boolean execute() {
+					Command c = saveQueue.remove( 0 );
+					c.execute();
+					return saveQueue.size()>0;
+				}
+			}, saveTimeout);
+		}
+	}
 }
