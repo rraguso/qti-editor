3357ddac497b4846af1b2b688109711741568404
ulff.pl <ulff.pl@b23be626-c637-cee4-8df3-8a3a905a95a0>
2010-09-27 13:13:57 +0000

    added style declatarion to extended_valid_elements bug 37222

--- trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tinymce_init.js
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tinymce_init.js
@@ -25,7 +25,7 @@ tinyMCE.init({
 		+"correctResponse,value,itemBody,applink[lid|title],mapping[defaultValue],mapEntry[mapKey|mappedValue],"
 		+"qy:comment[idref],feedbackInline[identifier|showHide],modalFeedback[outcomeIdentifier|identifier|showHide|sound|senderIdentifier|style],"
 		+"math[title|xmlns],mstyle[mathsize|mathcolor|fontfamily|displaystyle],mfrac,mrow,mi,mo,mn,msup,mroot,munder,"
-		+"mtable,mtr,mtd,mspace,changesTracking[state]",
+		+"mtable,mtr,mtd,mspace,changesTracking[state],styleDeclaration,link[href|userAgent]",
 	
 	handle_event_callback : "actionOnQTI",
 	convert_fonts_to_spans : true,
