99dca55719f6e2ac5fa23cbef24a93d3b3e6abc7
dawid.bartel85 <dawid.bartel85@b23be626-c637-cee4-8df3-8a3a905a95a0>
2011-08-23 16:12:49 +0000

    Dodanie nowego pluginu bug 48491

---
+++ QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_gapinlinechoice/langs/en_dlg.js
@@ -0,0 +1,33 @@
+tinyMCE.addI18n('en.gapinlinechoice_dlg',{
+	window_title:"Insert / modify gap / inline choice section",
+	question_label:"Question",
+	question_content:"Content",
+	exercise_label:"Exercise",
+	exercise_content:"Content",
+	gaps_label:"Gaps",
+	gap_correct_answer:"Correct answer",
+	gap_dropdown:"Drop down",
+	gap_distractor:"Distractors",
+	gap_apply_gap:"Apply gap",
+	gap_remove_gap:"Remove gap",
+	gap_remove:"Remove",
+	gap_add:"Add",
+	gapinlinechoice_add_new_gap:"Add new gap",
+	gapinlinechoice_insert:"Insert",
+	gapinlinechoice_cancel:"Cancel",
+});
+
+tinyMCE.addI18n('en.inlinechoice_dlg',{
+	window_title:"Insert / modify inline choice element",
+	modify:"Modify",
+	insert:"Insert",
+	remove:"Remove",
+	answers_label:"Answers",
+	answer_content:"Answer content",
+	answer_points:"Correct",
+	answer_fixed:"Fixed",
+	add_answer_button:"Add answer",
+	remove_answer_button:"Remove",
+	question_data:"Question metadata",
+	question_shuffle:"Shuffle"
+});
