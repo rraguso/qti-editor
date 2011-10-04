9e020f00e9de8dffd5822c9812ce610851570e0f
dawid.bartel85 <dawid.bartel85@b23be626-c637-cee4-8df3-8a3a905a95a0>
2011-10-04 19:22:36 +0000

    string Encode dla page title bug 51286

--- trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_pagetitle/js/pagetitle.js
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_pagetitle/js/pagetitle.js
@@ -12,7 +12,7 @@ var pagetitleDialog = {
 
 	insertPageTitle: function(form) {
 		var ed = tinymce.EditorManager.activeEditor;
-		var title = form.pagetitle.value;
+		var title = stringEncode(form.pagetitle.value);
 		ed.dom.doc.body.innerHTML = ed.dom.doc.body.innerHTML.replace(/(<assessmentItem.*title=")([^"]*)(".*>)/gi,"$1"+title+"$3");
 		tinyMCEPopup.close();
 		return true;
--- trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_pagetitle/js/util.js
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_pagetitle/js/util.js
@@ -1,5 +1,5 @@
 function stringEncode(text) {
-	return $('<div/>').text(text).html();
+	return $('<div/>').text(text).html().replace(/"/g, "&quot;");
 }
 function stringDecode(text) {
 	return $('<div/>').html(text).text();
