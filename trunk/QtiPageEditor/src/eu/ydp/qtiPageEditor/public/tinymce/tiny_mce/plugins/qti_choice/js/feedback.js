6de29c97fcf0781260db02bbb86a825d038c1290
dawid.bartel85 <dawid.bartel85@b23be626-c637-cee4-8df3-8a3a905a95a0>
2011-11-03 20:32:48 +0000

    Obsługa znaków < i > dla feedbacków bug 50185

--- trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_choice/js/feedback.js
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_choice/js/feedback.js
@@ -8,10 +8,10 @@ var feedbackDialog = {
 		document.getElementById('identifier').setAttribute('value',data.identifier);
 		document.getElementById('exerciseid').setAttribute('value',data.exerciseid);
 		if(data.feedback != undefined && data.feedback != '') {
-			document.getElementById('feedback').setAttribute('value',data.feedback);
+			document.getElementById('feedback').setAttribute('value',stringDecode(data.feedback));
 			document.getElementById('fdb_sound').setAttribute('value',data.feedback_sound);
 		} else if(tinyMCE.feedback != undefined && tinyMCE.feedback[data.identifier] != undefined) {
-			document.getElementById('feedback').setAttribute('value',tinyMCE.feedback[data.identifier].text);
+			document.getElementById('feedback').setAttribute('value',stringDecode(tinyMCE.feedback[data.identifier].text));
 			document.getElementById('fdb_sound').setAttribute('value',tinyMCE.feedback[data.identifier].sound);
 		}
 		
@@ -49,7 +49,7 @@ var feedbackDialog = {
 		if(tinyMCE.feedback[exerciseid] == undefined) {
 			tinyMCE.feedback[exerciseid] = {text: new Array, sound: new Array};
 		}
-		tinyMCE.feedback[exerciseid].text[identifier] = feedback;
+		tinyMCE.feedback[exerciseid].text[identifier] = stringEncode(feedback);
 		tinyMCE.feedback[exerciseid].sound[identifier] = feedback_sound;
 		
 		tinyMCEPopup.close();
