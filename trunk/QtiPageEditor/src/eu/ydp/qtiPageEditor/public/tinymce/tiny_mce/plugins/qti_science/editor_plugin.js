063593815f45626a0e21309fb42f32fcd3e7093e
dawid.bartel85@gmail.com <dawid.bartel85@gmail.com@b23be626-c637-cee4-8df3-8a3a905a95a0>
2012-04-25 19:31:44 +0000

    57199 - Inicjalne wrzucenie kodów związanych z modułem naukowym

---
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_science/editor_plugin.js
@@ -0,0 +1,67 @@
+(function() {
+	// Load plugin specific language pack
+	tinymce.PluginManager.requireLangPack('science');
+
+	tinymce.create('tinymce.plugins.SciencePlugin', {
+
+		init : function(ed, url) {
+
+			ed.addCommand('mceScience', function(ui, data) {
+				ed.windowManager.open({
+					file : url + '/science.htm',
+					width : 650,
+					height : 300,
+					inline : 1
+				}, {
+					plugin_url : url,
+					mathXml : data
+				});
+			});
+
+			// Register example button
+			ed.addButton('science', {
+				title : 'science.desc',
+				cmd : 'mceScience',
+				image : url + '/img/scienceButton.png'
+			});
+
+			// Add a node change handler, selects the button in the UI when a image is selected
+			ed.onNodeChange.add(function(ed, cm, n) {
+				//cm.setActive('example', n.nodeName == 'IMG');
+			});
+		},
+
+		/**
+		 * Creates control instances based in the incomming name. This method is normally not
+		 * needed since the addButton method of the tinymce.Editor class is a more easy way of adding buttons
+		 * but you sometimes need to create more complex controls like listboxes, split buttons etc then this
+		 * method can be used to create those.
+		 *
+		 * @param {String} n Name of the control to create.
+		 * @param {tinymce.ControlManager} cm Control manager to use inorder to create new control.
+		 * @return {tinymce.ui.Control} New control instance or null if no control was created.
+		 */
+		createControl : function(n, cm) {
+			return null;
+		},
+
+		/**
+		 * Returns information about the plugin as a name/value array.
+		 * The current keys are longname, author, authorurl, infourl and version.
+		 *
+		 * @return {Object} Name/value array containing information about the plugin.
+		 */
+		getInfo : function() {
+			return {
+				longname : 'Plugin for creating and managing math and science formula',
+				author : '<a target="_blank" href="http://www.ydp.eu">Young Digital Planet</a>',
+				authorurl : '',
+				infourl : '',
+				version : "1.1"
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('qti_science', tinymce.plugins.SciencePlugin);
+})();
