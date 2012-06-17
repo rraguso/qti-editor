443e71e0c7fc6b8bf43a65c19015c612e891b3ea
dawid.bartel85@gmail.com <dawid.bartel85@gmail.com@b23be626-c637-cee4-8df3-8a3a905a95a0>
2012-06-17 12:07:16 +0000

    Coś nie tak z branchpointem, kolejna próba przygotowania kodu do branchowania

--- trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tinymce_qti_utils.js
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tinymce_qti_utils.js
@@ -334,8 +334,6 @@ function textInteractionsGroupContentToQTI(content, spanBegun) {
 					text += xh.prepareNodeBegin(n);
 					text += textInteractionsGroupContentToQTI(n, spanBegun);
 					text += xh.prepareNodeEnd(n);
-				} else if ('IMG' == n.tagName) {
-					text += mediaInteractionToQTI(n);
 				} else {
 					text += parseMathHTML2QTI(xh.prepareNode(n));
 				}
@@ -471,13 +469,7 @@ function choiceInteractionToQTI(ci) {
 function mediaInteractionToQTI(mi) {
 	var text = '';
 	var xh = tinymce.EditorManager.activeEditor.XmlHelper;
-	var mediaNode = null;
-	
-	if (null != mi.firstChild) {
-		mediaNode = mi.firstChild;
-	} else {
-		mediaNode = mi;
-	}
+	var mediaNode = mi.firstChild;
 
 	if ('IMG' == mediaNode.tagName) {
 		var src = mediaNode.getAttribute('src');
@@ -1014,7 +1006,7 @@ function actionOnQTI(e) {
 				}*/
 				
 				// MediaLib
-				if((selectedNode.nodeName == 'IMG' && selectedNode.parentNode.id != 'gapInlineChoiceInteractionContent') || (selectedNode.nodeName == 'FIELDSET' && selectedNode.id == 'runFileUploadLib')) {
+				if(selectedNode.nodeName == 'IMG' || (selectedNode.nodeName == 'FIELDSET' && selectedNode.id == 'runFileUploadLib')) {
 					runMediaLib(selectedNode);
 					break;
 				}
