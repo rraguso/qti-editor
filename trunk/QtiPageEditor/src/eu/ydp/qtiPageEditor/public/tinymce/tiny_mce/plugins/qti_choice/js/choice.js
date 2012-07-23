3364d2fb39c0b9c5d6ae6d53021a456706adf815
dawid.bartel85@gmail.com <dawid.bartel85@gmail.com@b23be626-c637-cee4-8df3-8a3a905a95a0>
2012-07-23 18:56:07 +0000

    66431 - poprawka obsługi modułu matematycznego w pluginie choice

--- trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_choice/js/choice.js
+++ trunk/QtiPageEditor/src/eu/ydp/qtiPageEditor/public/tinymce/tiny_mce/plugins/qti_choice/js/choice.js
@@ -75,6 +75,7 @@ var choiceDialog = {
 				InputHelper.init($("#answer_"+q));
 				if (f.images.checked){
 					document.getElementById("taginsert_menu_answer_"+q).style.display = 'none';
+					$(".taginsert_math").hide();
 				}
 				/*
 				if(tinyMCE.feedback == undefined) {
